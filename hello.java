b9b14f82ef4275fba5486159b3e16c16aaca41b1
xipecast <72638323+xipecast@users.noreply.github.com>
2025-09-16 11:45:50 -0500

    initial version

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+
+  public static void main(String[] args) {
+    // generate ouput
+    String firstLine = ("Hello, again");
+    System.out.println("The value of firstLine is ");
+    System.out.println("firstline");
+  }
+
+}
